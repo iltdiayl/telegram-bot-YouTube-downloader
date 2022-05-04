1a59f0d49d61853a2eb343f970562263643d5d60
iltdiayl <98454130+iltdiayl@users.noreply.github.com>
2022-05-04 10:45:20 +0300

    Add files via upload

---
+++ yt_telegram.py
@@ -0,0 +1,31 @@
+import telebot
+from pytube import YouTube
+
+bot = telebot.TeleBot("5151650799:AAEjvrQ7oJuM8vz1PsxV0qQAHy1Zt1jsdJk")
+
+DOWNLOAD_FOLDER = "E:/Video"
+
+a = " "
+video_obj = " "
+stream = " "
+
+@bot.message_handler(commands=['start', "help"])
+def send_welcome(message):
+	
+
+	bot.send_message(message.chat.id, "Submit a link to a video on YouTube 😀. \nLike this, for example: https://www.youtube.com/watch?v=dQw4w9WgXсQ.")
+	
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	global a
+	global video_obj
+	
+	global stream 
+	bot.send_message(message.chat.id, "Please, wait...")	
+	video_obj = YouTube(message.text)
+	stream = video_obj.streams.get_highest_resolution()
+	stream.download(output_path=DOWNLOAD_FOLDER, filename='yt_video.mp4')
+	video = open('E:/Video/yt_video.mp4', 'rb')
+	bot.send_video(message.chat.id, video, timeout = 200)
+
+bot.infinity_polling() 
